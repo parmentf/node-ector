f765a2dab0349b334ad62929f469b51e2cab135e
François Parmentier <francois.parmentier@gmail.com>
2017-10-09 22:14:16 +0200

    Move ector_test to async / await

--- __tests__/ector_test.js
+++ __tests__/ector_test.js
@@ -147,201 +147,171 @@ describe('Bot', () => {
         });
       });
 
-      it('should create a sentence node', () => {
+      it('should create a sentence node', async () => {
         var ector = Ector();
-        return ector.addEntry('Hello.').then(nodes => {
-          expect(ector.cn.node[1].label).toBe('Hello.');
-          expect(ector.cn.node[1].type).toBe('s');
-        });
+        const nodes = await ector.addEntry('Hello.');
+        expect(ector.cn.node[1].label).toBe('Hello.');
+        expect(ector.cn.node[1].type).toBe('s');
       });
 
-      it('should return an array of one word node', () => {
+      it('should return an array of one word node', async () => {
         var ector = Ector();
-        return ector.addEntry('Hello.').then(nodes => {
-          expect(nodes).not.toBeInstanceOf(Error);
-          expect(nodes).toHaveLength(1);
-          expect(nodes[0].label).toBe('Hello.');
-          expect(nodes[0].type).toBe('w');
-        });
+        const nodes = await ector.addEntry('Hello.');
+        expect(nodes).not.toBeInstanceOf(Error);
+        expect(nodes).toHaveLength(1);
+        expect(nodes[0].label).toBe('Hello.');
+        expect(nodes[0].type).toBe('w');
       });
 
-      it('should return an array of two word nodes', () => {
+      it('should return an array of two word nodes', async () => {
         var ector = Ector();
-        return ector.addEntry('Hello world.').then(nodes => {
-          expect(nodes).not.toBeInstanceOf(Error);
-          expect(nodes).toHaveLength(2);
-          expect(nodes[0].label).toBe('Hello');
-          expect(nodes[0].type).toBe('w');
-          expect(nodes[1].label).toBe('world.');
-          expect(nodes[1].type).toBe('w');
-        });
+        const nodes = await ector.addEntry('Hello world.');
+        expect(nodes).not.toBeInstanceOf(Error);
+        expect(nodes).toHaveLength(2);
+        expect(nodes[0].label).toBe('Hello');
+        expect(nodes[0].type).toBe('w');
+        expect(nodes[1].label).toBe('world.');
+        expect(nodes[1].type).toBe('w');
       });
 
-      it('should add the nodes in the concept network', () => {
+      it('should add the nodes in the concept network', async () => {
         var ector = Ector();
         var cn = ector.cn;
         expect(cn).not.toBeNull();
-        return ector.addEntry('Hello world.').then(nodes => {
-          expect(cn.node).toHaveLength(4);
-        });
+        await ector.addEntry('Hello world.');
+        expect(cn.node).toHaveLength(4);
       });
 
-      it('should add positions in the sentence', () => {
+      it('should add positions in the sentence', async () => {
         var ector = Ector();
-        return ector.addEntry('Salut tout le monde.').then(nodes => {
-          expect(nodes[0].beg).toBe(1);
-          expect(nodes[2].mid).toBe(1);
-          expect(nodes[3].end).toBe(1);
-        });
+        const nodes = await ector.addEntry('Salut tout le monde.');
+        expect(nodes[0].beg).toBe(1);
+        expect(nodes[2].mid).toBe(1);
+        expect(nodes[3].end).toBe(1);
       });
 
-      it('should increment positions in the sentence', () => {
+      it('should increment positions in the sentence', async () => {
         var ector = Ector();
-        return ector
-          .addEntry('Salut tout le monde.')
-          .then(nodes => {
-            return ector.addEntry('Salut le peuple du monde.');
-          })
-          .then(nodes2 => {
-            expect(nodes2[0].beg).toBe(2);
-            expect(nodes2[1].mid).toBe(2);
-            expect(nodes2[2].mid).toBe(1);
-            expect(nodes2[4].end).toBe(2);
-          });
+        await ector.addEntry('Salut tout le monde.');
+        const nodes2 = await ector.addEntry('Salut le peuple du monde.');
+        expect(nodes2[0].beg).toBe(2);
+        expect(nodes2[1].mid).toBe(2);
+        expect(nodes2[2].mid).toBe(1);
+        expect(nodes2[4].end).toBe(2);
       });
 
-      it('should add links between following tokens', () => {
+      it('should add links between following tokens', async () => {
         var ector = Ector();
         var cn = ector.cn;
-        return ector.addEntry('Salut tout le monde.').then(nodes => {
-          expect(cn.link['2_3']).toEqual({
-            fromId: 2,
-            toId: 3,
-            coOcc: 1
-          });
-          expect(cn.link['3_4']).toEqual({
-            fromId: 3,
-            toId: 4,
-            coOcc: 1
-          });
-          expect(cn.link['4_5']).toEqual({
-            fromId: 4,
-            toId: 5,
-            coOcc: 1
-          });
+        await ector.addEntry('Salut tout le monde.');
+        expect(cn.link['2_3']).toEqual({
+          fromId: 2,
+          toId: 3,
+          coOcc: 1
+        });
+        expect(cn.link['3_4']).toEqual({
+          fromId: 3,
+          toId: 4,
+          coOcc: 1
+        });
+        expect(cn.link['4_5']).toEqual({
+          fromId: 4,
+          toId: 5,
+          coOcc: 1
         });
       });
 
-      it('should add links between sentence and its tokens', () => {
+      it('should add links between sentence and its tokens', async () => {
         var ector = Ector();
         var cn = ector.cn;
-        return ector.addEntry('Salut tout le monde.').then(nodes => {
-          expect(cn.link['1_2']).toEqual({ fromId: 1, toId: 2, coOcc: 1 });
-          expect(cn.link['1_3']).toEqual({ fromId: 1, toId: 3, coOcc: 1 });
-          expect(cn.link['1_4']).toEqual({ fromId: 1, toId: 4, coOcc: 1 });
-          expect(cn.link['1_5']).toEqual({ fromId: 1, toId: 5, coOcc: 1 });
-        });
+        await ector.addEntry('Salut tout le monde.');
+        expect(cn.link['1_2']).toEqual({ fromId: 1, toId: 2, coOcc: 1 });
+        expect(cn.link['1_3']).toEqual({ fromId: 1, toId: 3, coOcc: 1 });
+        expect(cn.link['1_4']).toEqual({ fromId: 1, toId: 4, coOcc: 1 });
+        expect(cn.link['1_5']).toEqual({ fromId: 1, toId: 5, coOcc: 1 });
       });
 
-      it('should add links between following sentences', () => {
+      it('should add links between following sentences', async () => {
         var ector = Ector();
         var cn = ector.cn;
-        return ector.addEntry('Ah! Oh.').then(nodes => {
-          expect(cn.link['1_3']).toEqual({ fromId: 1, toId: 3, coOcc: 1 });
-        });
+        await ector.addEntry('Ah! Oh.');
+        expect(cn.link['1_3']).toEqual({ fromId: 1, toId: 3, coOcc: 1 });
       });
 
-      it('should increment links between following tokens', () => {
+      it('should increment links between following tokens', async () => {
         var ector = Ector();
         var cn = ector.cn;
-        return ector
-          .addEntry('Salut tout le monde.')
-          .then(nodes => {
-            expect(cn.link['2_3']).toEqual({ fromId: 2, toId: 3, coOcc: 1 });
-            return ector.addEntry('Salut tout le peuple.');
-          })
-          .then(nodes => {
-            expect(cn.link['2_3']).toEqual({ fromId: 2, toId: 3, coOcc: 2 });
-          });
+        await ector.addEntry('Salut tout le monde.');
+        expect(cn.link['2_3']).toEqual({ fromId: 2, toId: 3, coOcc: 1 });
+        await ector.addEntry('Salut tout le peuple.');
+        expect(cn.link['2_3']).toEqual({ fromId: 2, toId: 3, coOcc: 2 });
       });
 
-      it('should create tokens from second sentence', () => {
+      it('should create tokens from second sentence', async () => {
         var ector = Ector();
         var cn = ector.cn;
-        return ector.addEntry('Salut. Hello.').then(nodes => {
-          expect(cn.node).toHaveLength(5);
-        });
+        await ector.addEntry('Salut. Hello.');
+        expect(cn.node).toHaveLength(5);
       });
 
-      it('should create node for second sentence', () => {
+      it('should create node for second sentence', async () => {
         var ector = Ector();
         var cn = ector.cn;
-        return ector.addEntry('Salut. Hello.').then(nodes => {
-          expect(Object.keys(cn.nodeIndex)).toContain('sHello.');
-        });
+        await ector.addEntry('Salut. Hello.');
+        expect(Object.keys(cn.nodeIndex)).toContain('sHello.');
       });
 
-      it('should count Hello as a beginning node.', () => {
+      it('should count Hello as a beginning node.', async () => {
         var ector = Ector();
         var cn = ector.cn;
-        return ector.addEntry('Salut. Hello.').then(nodes => {
-          expect(cn.node[4].beg).toBeInstanceOf(Number);
-          expect(cn.node[4].beg).toBe(1);
-          expect(cn.node[3].beg).toBeInstanceOf(Number);
-          expect(cn.node[3].beg).toBe(1);
-        });
+        await ector.addEntry('Salut. Hello.');
+        expect(cn.node[4].beg).toBeInstanceOf(Number);
+        expect(cn.node[3].beg).toBeInstanceOf(Number);
+        expect(cn.node[3].beg).toBe(1);
       });
     });
 
     describe('in the ConceptNetworkState', () => {
       var ector, cn, cns;
 
-      beforeAll(() => {
+      beforeAll(async () => {
         ector = Ector();
         cn = ector.cn;
         cns = ConceptNetworkState(cn);
-        return ector.addEntry('Sentence one.', cns);
+        await ector.addEntry('Sentence one.', cns);
       });
 
       describe('with a two-tokens sentence', () => {
-        beforeAll(() => {
-          return ector.addEntry('Sentence one.', cns);
+        beforeAll(async () => {
+          await ector.addEntry('Sentence one.', cns);
         });
 
-        it('should activate the sentence node', () => {
-          return cns.getActivationValue(1).then(value => {
-            expect(value).toBe(100);
-          });
+        it('should activate the sentence node', async () => {
+          const value = await cns.getActivationValue(1);
+          expect(value).toBe(100);
         });
 
-        it('should activate the token node', () => {
-          return cns
-            .getActivationValue(2)
-            .then(value2 => {
-              expect(value2).toBe(100);
-              return cns.getActivationValue(3);
-            })
-            .then(value3 => {
-              expect(value3).toBe(100);
-            });
+        it('should activate the token node', async () => {
+          const value2 = await cns.getActivationValue(2);
+          expect(value2).toBe(100);
+          const value3 = await cns.getActivationValue(3);
+          expect(value3).toBe(100);
         });
       });
 
       describe('with a one-token sentence', () => {
-        beforeAll(() => {
-          return ector.addEntry('Sentence.', cns);
+        beforeAll(async () => {
+          await ector.addEntry('Sentence.', cns);
         });
 
-        it('should activate the sentence node', () => {
-          return cns.getActivationValue(5).then(value => {
-            expect(value).toBe(100);
-          });
+        it('should activate the sentence node', async () => {
+          const value = await cns.getActivationValue(5);
+          expect(value).toBe(100);
         });
 
-        it('should activate the token node', () => {
-          return cns.getActivationValue(6).then(value => {
-            expect(value).toBe(100);
-          });
+        it('should activate the token node', async () => {
+          const value = await cns.getActivationValue(6);
+          expect(value).toBe(100);
         });
       });
     });
@@ -349,68 +319,61 @@ describe('Bot', () => {
 
   describe('Response', () => {
     // First time, only the same sentence is returned.
-    it('should generate a response similar to the stimulus', () => {
-      var ector = Ector('ECTOR', 'Guy');
-      return ector.addEntry('Hello ECTOR.').then(nodes => {
-        var response = ector.generateResponse();
-        expect(response).toEqual({ sentence: 'Hello Guy.', nodes: [2, 3] });
-      });
+    it('should generate a response similar to the stimulus', async () => {
+      const ector = Ector('ECTOR', 'Guy');
+      await ector.addEntry('Hello ECTOR.');
+      const response = ector.generateResponse();
+      expect(response).toEqual({ sentence: 'Hello Guy.', nodes: [2, 3] });
     });
 
-    it('should replace both names', () => {
-      var ector = Ector('ECTOR', 'Guy');
-      return ector.addEntry('Hello ECTOR and ECTOR.').then(nodes => {
-        var response = ector.generateResponse();
-        expect(response.sentence).toBe('Hello Guy and Guy.');
-      });
+    it('should replace both names', async () => {
+      const ector = Ector('ECTOR', 'Guy');
+      await ector.addEntry('Hello ECTOR and ECTOR.');
+      const response = ector.generateResponse();
+      expect(response.sentence).toBe('Hello Guy and Guy.');
     });
   });
 
   describe('PreviousSentenceNodeId', () => {
-    it('should store the last sentence node id', () => {
-      var ector = Ector('ECTOR', 'Guy');
+    it('should store the last sentence node id', async () => {
+      const ector = Ector('ECTOR', 'Guy');
       expect(ector.lastSentenceNodeId).toBeNull();
-      return ector.addEntry('Hello ECTOR.').then(nodes => {
-        expect(ector.lastSentenceNodeId).toEqual(1);
-      });
+      await ector.addEntry('Hello ECTOR.');
+      expect(ector.lastSentenceNodeId).toEqual(1);
     });
 
     describe('link nodes to lastSentenceNode', () => {
-      it('should not create links when no lastSentenceNode exist', () => {
-        var ector = Ector('ECTOR', 'Guy');
-        return ector.addEntry('Hello.').then(nodes => {
-          ector.lastSentenceNodeId = null;
-          expect(() => {
-            ector.linkNodesToLastSentence([2]);
-          }).toThrow(Error);
-        });
-      });
-
-      it('should not create link when null is given', () => {
-        var ector = Ector('ECTOR', 'Guy');
-        return ector.addEntry('Hello.').then(nodes => {
-          ector.linkNodesToLastSentence(null);
-          expect(ector.cn.link['2_']).toBeUndefined();
-        });
-      });
-
-      it('should not create link when [] is given', () => {
-        var ector = Ector('ECTOR', 'Guy');
-        return ector.addEntry('Hello.').then(nodes => {
-          ector.linkNodesToLastSentence([]);
-          expect(ector.cn.link['2_']).toBeUndefined();
-        });
-      });
-
-      it('should create links', () => {
-        var ector = Ector('ECTOR', 'Guy');
-        return ector.addEntry('Hello.').then(nodes => {
-          // 1 link from sentenceNode to the only tokenNode
-          // sHello. (1) -> wHello. (2)
-          expect(ector.cn.link['2_1']).toBeUndefined();
+      it('should not create links when no lastSentenceNode exist', async () => {
+        const ector = Ector('ECTOR', 'Guy');
+        await ector.addEntry('Hello.');
+        ector.lastSentenceNodeId = null;
+        expect(() => {
           ector.linkNodesToLastSentence([2]);
-          expect(ector.cn.link['2_1']).toBeInstanceOf(Object);
-        });
+        }).toThrow(Error);
+      });
+
+      it('should not create link when null is given', async () => {
+        const ector = Ector('ECTOR', 'Guy');
+        await ector.addEntry('Hello.');
+        ector.linkNodesToLastSentence(null);
+        expect(ector.cn.link['2_']).toBeUndefined();
+      });
+
+      it('should not create link when [] is given', async () => {
+        const ector = Ector('ECTOR', 'Guy');
+        await ector.addEntry('Hello.');
+        ector.linkNodesToLastSentence([]);
+        expect(ector.cn.link['2_']).toBeUndefined();
+      });
+
+      it('should create links', async () => {
+        const ector = Ector('ECTOR', 'Guy');
+        await ector.addEntry('Hello.');
+        // 1 link from sentenceNode to the only tokenNode
+        // sHello. (1) -> wHello. (2)
+        expect(ector.cn.link['2_1']).toBeUndefined();
+        ector.linkNodesToLastSentence([2]);
+        expect(ector.cn.link['2_1']).toBeInstanceOf(Object);
       });
     });
   });
@@ -444,12 +407,11 @@ describe('Injector', () => {
     }).toThrow(Error);
   });
 
-  it('should work as a normal ConceptNetwork', () => {
+  it('should work as a normal ConceptNetwork', async () => {
     ector.ConceptNetwork = StrangeConceptNetwork;
-    return ector.addEntry('Hello ECTOR.').then(nodes => {
-      var res = ector.generateResponse();
-      expect(res.sentence).toBe('Hello Guy.');
-    });
+    await ector.addEntry('Hello ECTOR.');
+    const res = ector.generateResponse();
+    expect(res.sentence).toBe('Hello Guy.');
   });
 
   it('should have its supplemental methods', () => {
